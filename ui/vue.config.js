539a92cc9971abc13988e8db2c9b8923bf611e75
kodah <kodah@pull.dev>
2021-01-05 02:03:26 -0600

    Fix Vue config

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,4 @@
 const { useGuarkLockFile, checkBeforeBuild } = require('guark/build')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 checkBeforeBuild()
 
@@ -15,18 +14,6 @@ module.exports =
                   after: (app, server, compiler) => compiler.hooks.done.tap("Guark", useGuarkLockFile)
                 },
               mode: 'development',
-              module: {
-                  rules: [
-                      { test: /\.vue$/, include: /src/, loader: 'vue-loader', options: { loaders: { js: 'awesome-typescript-loader?silent=true', loader: "unicode-loader" } } },
-                      // { test: /\.ts$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
-                      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-                      { test: /\.(png|jpg|jpeg|gif|svg|woff2|woff)$/, include: /src/, use: 'url-loader?limit=25000' }
-                  ]
-              },
-              plugins: [
-                  // make sure to include the plugin for the magic
-                  new VueLoaderPlugin()
-              ]
           },
         transpileDependencies: [
           "vuetify"
